ed89d2c0e4ebc12d37b6cebeb89a4e46c06bb6c0
SystematicGold <nno906952@gmail.com>
2022-12-05 15:10:08 +0300

    Commit

--- source Code/GoldSystem/Main/Forms/FrmMain.vb
+++ source Code/GoldSystem/Main/Forms/FrmMain.vb
@@ -6,4 +6,12 @@ Public Class FrmMain
       MessageBox.Show(ex.ToString())
     End Try
   End Sub
+
+  Private Sub Guna2Button4_Click(sender As Object, e As EventArgs) Handles Guna2Button4.Click, Guna2Button5.Click
+
+  End Sub
+
+  Private Sub Guna2Panel5_Paint(sender As Object, e As PaintEventArgs) Handles Guna2Panel5.Paint
+
+  End Sub
 End Class
