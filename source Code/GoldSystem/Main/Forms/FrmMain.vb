c57c17f6044f8fe2bb82e299b49a3de9b892f65d
SystematicGold <nno906952@gmail.com>
2022-12-12 14:52:49 +0300

    Commit

--- source Code/GoldSystem/Main/Forms/FrmMain.vb
+++ source Code/GoldSystem/Main/Forms/FrmMain.vb
@@ -10,68 +10,29 @@ Public Class FrmMain
   End Sub
   Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     Try
-            'WebBrowser1.Navigate("http://goldpricez.com/kw/18k/gram")
-        Catch ex As Exception
-      MessageBox.Show(ex.ToString())
-    End Try
-  End Sub
-<<<<<<< HEAD
-  Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs) Handles WebBrowser1.DocumentCompleted
-    Try
-      Dim T0 As String = "gold_price_table"
-      Dim K18 As String
-      Guna2TextBox1.Text = WebBrowser1.Document.GetElementById("gold_price_table").InnerText
+      'WebBrowser1.Navigate("http://goldpricez.com/kw/18k/gram")
+      PanelAll.Visible = True
+      PanalGold.Visible = False
     Catch ex As Exception
       MessageBox.Show(ex.ToString())
     End Try
   End Sub
-=======
-    Private Sub WebBrowser1_DocumentCompleted(sender As Object, e As WebBrowserDocumentCompletedEventArgs)
-        Try
-            'Dim T0 As String = "gold_price_table"
-            'Dim K18 As String
-            'Guna2TextBox1.Text = WebBrowser1.Document.GetElementById("gold_price_table").InnerText
-
-
-        Catch ex As Exception
-            MessageBox.Show(ex.ToString())
-        End Try
-    End Sub
-    Private Sub Guna2GradientButton1_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton1.Click
-        Try
-            Dim F As New FrmGold
-            F.show()
-        Catch ex As Exception
-            MessageBox.Show(ex.ToString())
+  Private Sub Guna2GradientButton1_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton1.Click
+    Try
+      PanalGold.Visible = True
+    Catch ex As Exception
+      MessageBox.Show(ex.ToString())
         End Try
     End Sub
     Private Sub Guna2GradientButton2_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton2.Click
         Dim f As New XtraReport1
 
     End Sub
-
-    Private Sub PictureEdit1_Click(sender As Object, e As EventArgs)
-        Try
-            Guna2Panel7.Hide()
-        Catch ex As Exception
-
-        End Try
-    End Sub
-
-    Private Sub PictureEdit2_Click(sender As Object, e As EventArgs)
-        Try
-            'Guna2Panel6.Size.Width = 10.0
-        Catch ex As Exception
-
-        End Try
-    End Sub
-
-    Private Sub Guna2GradientButton4_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton4.Click
-
-    End Sub
-
-    Private Sub Guna2GradientButton5_Click(sender As Object, e As EventArgs) Handles Guna2GradientButton5.Click
-
-    End Sub
->>>>>>> 168439aafcdb12f7110aed5dd5cccf285cd81a2a
+  Private Sub PictureEdit1_Click_1(sender As Object, e As EventArgs) Handles PictureEdit1.Click
+    Try
+      PanalGold.Visible = False
+    Catch ex As Exception
+      MessageBox.Show(ex.ToString)
+    End Try
+  End Sub
 End Class
